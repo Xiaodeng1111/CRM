400783e7cf3bc271b5d3d25a34d52b4dffa18810
Xiaodeng1111 <chilegezhu@163.com>
2018-08-20 20:32:44 +0800

    second commit

---
+++ app/Http/Controllers/Index.php
@@ -0,0 +1,17 @@
+<?php 
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use View;
+use Memcache;
+use Illuminate\Support\Facades\Input;
+
+class Index extends Controller{
+
+	public function add(){
+
+	$post=Input::post();
+		
+	} 
+ }
